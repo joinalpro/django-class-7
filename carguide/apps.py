35c995d7ba40ad50f7eee9f8dadac827595ac47f
Joinal Abden <joinalzx@gmail.com>
2024-07-03 01:57:50 +0600

    django setup

---
+++ carguide/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarguideConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carguide'
---
+++ cartire/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CartireConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cartire'
